d0c1450f9f24722acd941c6a86f1025803b1702b
Theagns <markus.agnsgaard@hotmail.com>
2021-09-15 09:29:18 +0200

    database setup

--- src/test/java/facades/FacadeExampleTest.java
+++ src/test/java/facades/FacadeExampleTest.java
@@ -1,31 +1,30 @@
 package facades;
 
 import entities.Employee;
+import org.junit.jupiter.api.*;
 import utils.EMF_Creator;
 import entities.RenameMe;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 //Uncomment the line below, to temporarily disable this test
-//@Disabled
-public class FacadeExampleTest {
+@Disabled
+class EmployeeFacadeTest {
 
     private static EntityManagerFactory emf;
     private static EmployeeFacade facade;
 
-    public FacadeExampleTest() {
+    public EmployeeFacadeTest() {
     }
 
     @BeforeAll
     public static void setUpClass() {
-       emf = EMF_Creator.createEntityManagerFactoryForTest();
-       facade = EmployeeFacade.getEmployeeFacade(emf);
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+        facade = EmployeeFacade.getEmployeeFacade(emf);
     }
 
     @AfterAll
@@ -40,9 +39,10 @@ public class FacadeExampleTest {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-            em.createNamedQuery("RenameMe.deleteAllRows").executeUpdate();
-            em.persist(new RenameMe("Some txt", "More text"));
-            em.persist(new RenameMe("aaa", "bbb"));
+            em.createNamedQuery("Employee.deleteAllRows").executeUpdate();
+            em.persist(new Employee("Mathias","Ourøgade",10000));
+            em.persist(new Employee("Ellen", "Ourøgade",100));
+            em.persist(new Employee("Lars", "Fiktivgade",50000));
 
             em.getTransaction().commit();
         } finally {
@@ -55,11 +55,38 @@ public class FacadeExampleTest {
 //        Remove any data after each test was run
     }
 
-    // TODO: Delete or change this method 
     @Test
-    public void testAFacadeMethod() throws Exception {
-        //assertEquals(2, facade.getRenameMeCount(), "Expects two rows in the database");
+    void getEmployeeFacadeTest() {
     }
-    
+
+    @Test
+    void createTest() {
+        Employee employee = facade.create(new Employee("Jens","Aarhusgade",300));
+        assertEquals("Jens",employee.getName());
+    }
+
+    @Test
+    @Disabled //Reset id somehow?
+    void getEmployeeById() {
+        Employee employee = facade.getEmployeeById(1);
+        assertEquals("Ellen",employee.getName());
+    }
+
+    @Test
+    void getEmployeesByName() {
+        List<Employee> employees = facade.getEmployeesByName("Mathias");
+        assertEquals("Mathias",employees.get(0).getName());
+    }
+
+    @Test
+    void getEmployeesWithHighestSalary() {
+        List<Employee> employees = facade.getEmployeesWithHighestSalary();
+        assertEquals("Lars",employees.get(0).getName());
+    }
+
+    @Test
+    void getAllEmployeesTest() {
+        assertEquals(3,facade.getAll().size());
+    }
 
 }
